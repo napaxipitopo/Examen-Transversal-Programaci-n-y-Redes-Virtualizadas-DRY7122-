001c73732e8217b2693777a32084969be651bcf9
napaxipitopo <igna.catalan@duocuc.cl>
2025-07-03 05:55:00 +0000

    INTEGRANTE IACP

---
+++ INTEGRANTE_IACP.py
@@ -0,0 +1,11 @@
+# Lista de integrantes del grupo
+integrantes = [
+    "Ignacio Catalán",
+
+
+]
+
+# Imprimir cada integrante en la lista
+print("Integrantes del grupo:")
+for integrante in integrantes:
+    print("- " + integrante)
